02a2454e058c9829e5c679d239b71ad7ea0a309c
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-30 23:29:29 +0300

    Declare commands

---
+++ kuznetsov.petr/T3/commands.hpp
@@ -0,0 +1,18 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+#include <iosfwd>
+#include <vector>
+#include "polygon.hpp"
+
+namespace kuznetsov {
+  void area(std::istream&, std::ostream&, std::vector< Polygon >&);
+  void max(std::istream&, std::ostream&, std::vector< Polygon >&);
+  void min(std::istream&, std::ostream&, std::vector< Polygon >&);
+  void count(std::istream&, std::ostream&, std::vector< Polygon >&);
+  void same(std::istream&, std::ostream&, std::vector< Polygon >&);
+  void rects(std::istream&, std::ostream&, std::vector< Polygon >&);
+}
+
+
+#endif
+
