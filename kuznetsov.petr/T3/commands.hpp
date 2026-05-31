fd7d7667b6f6a9c59e12303d122f54a8c48a4d89
Kuznetsov Petr <kuznecovpetr502@yandex.ru>
2026-05-31 16:46:40 +0300

    Add CommandExecuter

--- kuznetsov.petr/T3/commands.hpp
+++ kuznetsov.petr/T3/commands.hpp
@@ -1,16 +1,27 @@
 #ifndef COMMAND_HPP
 #define COMMAND_HPP
 #include <iosfwd>
+#include <unordered_map>
 #include <vector>
+#include <string>
+#include <functional>
 #include "polygon.hpp"
 
 namespace kuznetsov {
-  void area(std::istream&, std::ostream&, const std::vector< Polygon >&);
-  void max(std::istream&, std::ostream&, const std::vector< Polygon >&);
-  void min(std::istream&, std::ostream&, const std::vector< Polygon >&);
-  void count(std::istream&, std::ostream&, const std::vector< Polygon >&);
-  void same(std::istream&, std::ostream&, const std::vector< Polygon >&);
-  void rects(std::istream&, std::ostream&, const std::vector< Polygon >&);
+  using polygons_t = std::vector< Polygon >;
+  using cmd_t = std::function< void(std::istream&, std::ostream&, const polygons_t&) >;
+  struct CommandExecuter {
+    std::unordered_map<std::string, cmd_t > cmds;
+    std::ostream& out;
+    const polygons_t& ps;
+  };
+  std::istream& operator>>(std::istream&, CommandExecuter& cmd);
+  void area(std::istream&, std::ostream&, const polygons_t&);
+  void max(std::istream&, std::ostream&, const polygons_t&);
+  void min(std::istream&, std::ostream&, const polygons_t&);
+  void count(std::istream&, std::ostream&, const polygons_t&);
+  void same(std::istream&, std::ostream&, const polygons_t&);
+  void rects(std::istream&, std::ostream&, const polygons_t&);
 }
 
 
